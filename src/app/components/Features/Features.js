2423ce2e562e34a11de2f2d628fe39f06ff940bb
Jhalakdev <108620433+Jhalakdev@users.noreply.github.com>
2024-01-20 20:16:54 +0530

    Update Features.js

--- src/app/components/Features/Features.js
+++ src/app/components/Features/Features.js
@@ -6,9 +6,9 @@ export default function Features() {
 
     return (<main className={featurecss.main}>
         <div className={featurecss.text}>
-            <p>Services We Provide</p>
-            <h1>WE DESIGN DIGITAL PRODUCTS THAT
-                HELP GROW BUSINESSES.</h1>
+            <p>Dienstleistungen, die wir anbieten</p>
+            <h1>WIR ENTWICKELN DIGITALE PRODUKTE, DIE
+                HELFEN SIE UNTERNEHMEN WACHSTUM.</h1>
         </div>
         <ServicesBox />
         
